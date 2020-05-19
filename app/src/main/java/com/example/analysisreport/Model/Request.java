09989edd22bfe046171e19a33d859eea1713a041
ASNProject <asnproject02@gmail.com>
2020-05-19 22:28:34 +0700

    Penambahan register

---
+++ app/src/main/java/com/example/analysisreport/Model/Request.java
@@ -0,0 +1,51 @@
+package com.example.analysisreport.Model;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+import java.io.Serializable;
+
+public class Request implements Serializable{
+
+    private String username, email, password;
+
+    public Request() {
+    }
+
+    public Request(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Request{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
